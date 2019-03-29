f6d071520b6277a50a14faeea5794741e666a084
Veetikki <veeti.ahvonen@gmail.com>
2019-03-29 20:40:41 +0200

    fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const {PythonShell} = require('python-shell');
 
-PythonShell.run('readTemperature.py', null, function (err, results) {
+PythonShell.run('readWeather.py', null, function (err, results) {
     if (err) throw err;
     console.log(results);
   });
