dd19af8435323b8c0a7f50d7d0da901ec086c947
Veetikki <veeti.ahvonen@gmail.com>
2019-03-29 12:22:11 +0200

    Now readTemparature.py is working, included testdata.csv

--- python/readTemperature.py
+++ python/readTemperature.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime as dt
 from time import sleep
 from envirophat import light, weather, motion, analog, leds
-import signal, asyncio
+import asyncio
 
 t = dt.datetime.now()
 conn = sqlite3.connect('weather.db')
@@ -11,13 +11,16 @@ def temp():
     return round(weather.temperature(),4)
 
 def press():
-    return weather.pressure(unit='hPa')
+    return round(weather.pressure(unit='hPa'),4)
 
-def insertData():
-    await asyncio.sleep(2)
-    data=[dt.datetime().now(), temp(), press()]
-    conn.execute("INSERT INTO WEATHER (DATE,TEMP,PRESS) \
-      VALUES (?, ?, ?)", data)
+async def insertData():
+    await asyncio.sleep(3)
+    data=[dt.datetime.now(), temp(), press()]
+    print(data)
+    conn.execute("INSERT INTO WEATHER (TIME,TEMP,PRESS) \
+            VALUES (?, ?, ?)", data)
+    conn.commit()
+    asyncio.ensure_future(insertData())
 
 
 loop = asyncio.get_event_loop()
@@ -32,4 +35,7 @@ def main():
         print("Closing station")
         loop.close()
 
-
+if __name__=="__main__":
+    main()
+    conn.close()
+    loop.close()
