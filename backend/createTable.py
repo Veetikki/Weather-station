a942cb4142a24b6f7923d99429a2804ab7b836e0
Veetikki <veeti.ahvonen@gmail.com>
2019-05-26 15:33:02 +0300

    fixed stuff and made new testData

--- backend/createTable.py
+++ backend/createTable.py
@@ -4,10 +4,11 @@ conn = sqlite3.connect('weather.db')
 print("Opened database successfully")
 
 conn.execute('''CREATE TABLE WEATHER
-         (DATE TEXT PRIMARY KEY NOT NULL,
-         CLOCK TEXT PRIMARY KEY NOT NULL,
+         (DATE TEXT NOT NULL,
+         CLOCK TEXT NOT NULL,
          TEMP REAL NOT NULL,
-         PRESS REAL NOT NULL);''')
+         PRESS REAL NOT NULL,
+         PRIMARY KEY (DATE, CLOCK));''')
 print("Table created successfully")
 
 conn.close()
--- backend/readWeather.py
+++ backend/readWeather.py
@@ -14,24 +14,29 @@ def press():
     return round(weather.pressure(unit='hPa'),4)
 
 async def insertData():
-    #await asyncio.sleep(3)
+    #Ajan säätö
+    #await asyncio.sleep(60)
     currentTime = dt.datetime.now()
     data=[currentTime.strftime("%Y-%m-%d"), currentTime.strftime("%H:%M") , temp(), press()]
     print(data)
     conn.execute("INSERT INTO WEATHER (DATE, CLOCK, TEMP, PRESS) \
             VALUES (?, ?, ?, ?)", data)
     conn.commit()
+    #jos haluaa tehdä ikuisesti
     #asyncio.ensure_future(insertData())
 
 loop = asyncio.get_event_loop()
 
 def main():
     try:
+        #kaksi ylintä jos ikuisesti alin riittää jos haluaa kerran tehdä
         #asyncio.ensure_future(insertData())
         #loop.run_forever()
         loop.run_until_complete(insertData())
-    except KeyboardInterrupt:
+    except  KeyboardInterrupt:
         print("Closing station")
+    finally: 
+        loop.stop()
         loop.close()
         conn.close()
 
